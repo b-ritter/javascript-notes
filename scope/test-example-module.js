04b0335677f5228f42b01770ddb4f64bc72db8de
RITTER <br440r@att.com>
2017-05-02 14:00:06 -0500

    Add test case for module method

--- scope/test-example-module.js
+++ scope/test-example-module.js
@@ -1,9 +1,13 @@
 const mocha = require('mocha');
-const blockscope = require('./example-module');
+const myModule = require('./example-module');
 const assert = require('chai').assert;
 
-describe('Foo', ()=> {
-    it("Should have a property 'foo' equal to 'bar'", ()=>{
-        assert.equal(blockscope.foo, 'bar');
+describe('Example Module', ()=> {
+    it("Should have a property 'foo' equal to 'bar'", ()=> {
+        assert.equal(myModule.foo, 'bar');
+    });
+
+    it("Should have a method 'baz' that returns 'foobar'", ()=> {
+        assert.equal(myModule.baz(), 'foobar');
     });
 });
